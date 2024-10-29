239d650605d1627f96d339f4f3c39f151d693757
Kayo Vinícius <kayo.vinicius034@gmail.com>
2024-10-28 23:17:14 -0300

    Calculo da Tinta para uma Parede
    Usuario digita o valor da lagura e da tinta. Com isso, o programa faz os calculos e exibe a quantidade necessaria para usar

---
+++ Guanabara/Aula 07/Ex_011.py
@@ -0,0 +1,6 @@
+print("---- CALCULAR ÁREA ----")
+largura = float(input("Digite o valor da largura: "))
+altura = float(input("Digite o valor da altura: "))
+metros = altura * largura
+tinta = metros/2
+print("Irão precisar de {:.2f} litros, para {:.2f} m²".format(tinta,metros))
