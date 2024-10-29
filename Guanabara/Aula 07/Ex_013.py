d0474f5262b051dd96709d2d1553f4ac4b5c79d6
Kayo Vinícius <kayo.vinicius034@gmail.com>
2024-10-28 23:12:56 -0300

    Salário com Aumento
    O usuário digita seu salário, e a seguir, exibiria na tela o aumento de 15% do salario dela

---
+++ Guanabara/Aula 07/Ex_013.py
@@ -0,0 +1,5 @@
+print("---- SALÁRIO COM 15% DE AUMENTO ----")
+salario = float(input("Digite o valor do seu salário: "))
+aumento = salario * 0.15
+valor = salario + aumento
+print("Seu salário será de R${} a partir de hoje".format(valor))
