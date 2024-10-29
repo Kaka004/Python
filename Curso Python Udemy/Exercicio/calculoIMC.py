b795c9bc5b49df08547eea903946da779023616a
Kayo Vinícius <kayo.vinicius034@gmail.com>
2024-10-29 17:49:17 -0300

    IMC
    Com os valores atribuidos na variaveis, o programa faz o calculo do imc e exibe na tela

---
+++ Curso Python Udemy/Exercicio/calculoIMC.py
@@ -0,0 +1,7 @@
+nome = 'Kayo'
+altura = 1.81
+peso = 105.0
+imc = peso / altura ** 2
+
+print("{} tem {} de altura.".format(nome, altura))
+print("Ele pesa {} kilos e seu IMC é {:.2f}".format(peso, imc))
