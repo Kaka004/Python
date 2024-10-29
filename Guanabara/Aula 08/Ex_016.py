4654c13ef94740dffe8dfdf68aea3de7467bc083
Kayo Vinícius <kayo.vinicius034@gmail.com>
2024-10-28 23:05:12 -0300

    Leitor Numérico de Função Inteira
    O usuário digita um numero flutuante, e com floor, ele reduz essas casas e retorna um numero inteiro, arredondando as casas decimais

---
+++ Guanabara/Aula 08/Ex_016.py
@@ -0,0 +1,8 @@
+from math import floor
+print("---- LEITOR NUMÉRICO DE FUNÇÃO INTEIRA ----")
+num = float(input("Digite um número: "))
+result = floor(num)
+print("O número {}, tem a porção inteira {}".format(num, result))
+
+
+
