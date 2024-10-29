748288be399885fb89d492079aee8e1d9a422a2b
Kayo Vinícius <kayo.vinicius034@gmail.com>
2024-10-29 16:57:18 -0300

    Sucessor e Antecessor
    Usuario digita um numero e, programa faz o calculo e aparece na tela o sucessor e o antecessor do numero

---
+++ Guanabara/Aula 07/aula_07.py
@@ -0,0 +1,5 @@
+print("----- SUCESSOR E ANTECESSOR -----")
+num = int(input("Digite um valor: "))
+sucessor = num + 1
+antecessor = num - 1
+print("Sucessor: {} \nAntecessor: {}".format(sucessor, antecessor))
