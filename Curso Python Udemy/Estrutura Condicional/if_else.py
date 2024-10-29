56cfec25eb7bb8d53019849de5467f0bb0e9a41b
Kayo Vinícius <kayo.vinicius034@gmail.com>
2024-10-29 17:51:04 -0300

    Treinamento IF-ELSE
    Treinamento com if-else

---
+++ Curso Python Udemy/Estrutura Condicional/if_else.py
@@ -0,0 +1,11 @@
+# if /    elif   / else
+# se / se não se/ se não
+
+entrada = input("Você quer 'entrar' ou 'sair'? ")
+
+if entrada == 'entrar':
+    print("Você está entrando no sistema")
+elif entrada == 'sair':
+    print("Você está saindo do sistema")
+else: 
+    print("Comando inválido")
