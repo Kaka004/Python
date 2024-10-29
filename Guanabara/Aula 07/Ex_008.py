5015b94f2d3e6dfc27d44b5077117e1dfe583c59
Kayo Vinícius <kayo.vinicius034@gmail.com>
2024-10-28 23:21:27 -0300

    Conversão em Metros
    Usuario digita um valor em metros, e a seguir, retorna o valor em centímetos e em milimetros

---
+++ Guanabara/Aula 07/Ex_008.py
@@ -0,0 +1,6 @@
+print("---- CONVERSÃO EM METROS ----")
+metro = float(input("Digite o valor de metragem: "))
+cent = metro * 100
+mili = metro * 1000
+
+print("Convertido em centímetros: {}\nConvertido em milímetros: {}".format(cent, mili))
