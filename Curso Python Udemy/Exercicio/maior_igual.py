7ecbb802067bd9e6d7ad3711c32a24ebf426aea1
Kayo Vinícius <kayo.vinicius034@gmail.com>
2024-10-29 17:12:15 -0300

    Maior, Igual ou Menor
    O usuario digita dois numeros e o progama exibe na tela se é maior, iguais, ou menor

---
+++ Curso Python Udemy/Exercicio/maior_igual.py
@@ -0,0 +1,9 @@
+num1 = input("Digite o primeiro valor: ")
+num2 = input("Digite o segundo valor: ")
+
+if num1 > num2:
+    print(f"{num1} é maior que {num2}")
+elif num1 == num2:
+    print("São iguais")
+else:
+    print(f"{num2} é maior que {num1}")
