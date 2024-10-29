870cda98557d0057399b14c76f6a2481f7aa0b19
Kayo Vinícius <kayo.vinicius034@gmail.com>
2024-10-29 17:06:45 -0300

    Meu Perfil 2
    Declaração das variaveis com meu nome, sobrenome, idade, se sou ou nao maior idade e minha altura

---
+++ Curso Python Udemy/Exercicio/perfil.py
@@ -0,0 +1,19 @@
+#Insira seu nome
+nome = 'Kayo'
+#Sobrenome
+sobrenome = 'Benjamin'
+#Sua idade
+idade = 20
+#Data de Nascimento
+anoNascimento = 2024 - idade
+#Se é de maior idade
+maiorIdade = idade >= 18
+#Sua Altura
+altura = 1.82
+#Imprime na tela
+print("Nome:", nome)
+print("Sobrenome:", sobrenome)
+print("Idade:", idade)
+print("Ano de nascimento:", anoNascimento)
+print("É maior de idade?", maiorIdade)
+print("Altura:", altura)
