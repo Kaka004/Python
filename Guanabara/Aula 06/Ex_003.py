838144015e5ea5ee02fabd247840c787b19cbd54
Kayo Vinícius <kayo.vinicius034@gmail.com>
2024-10-29 16:59:42 -0300

    Soma de Dois números
    O usuario digita 2 numeros, o programa faz a soma e exibe na tela

---
+++ Guanabara/Aula 06/Ex_003.py
@@ -0,0 +1,6 @@
+print("------ SOMA ENTRE NÚMEROS ------")
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+soma = num1 + num2
+print("A soma entre {} e {} é: {}".format(num1,num2, soma))
+
