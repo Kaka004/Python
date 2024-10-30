9a9f18336917143e65623c30e9718cfdaea64163
Kayo Vinícius <kayo.vinicius034@gmail.com>
2024-10-30 10:23:15 -0300

    Operadores
    Aula voltada em como executa como operador

---
+++ Curso Python Udemy/Aula Basica/operadores.py
@@ -0,0 +1,29 @@
+#Adição
+adicao = 10 + 10
+print(adicao)
+#Subtração
+subtracao = 10 - 5
+print(subtracao)
+#Multiplicação
+multiplicacao = 10 * 10
+print(multiplicacao)
+#Divisão
+divisao = 10 / 2.2 # Return float 
+print(divisao)
+#Divisão Inteira
+# Ele corta as casas decimais. Ent o resultado, no exemplo, será 4.0
+divisaoInteira = 10 // 2.2 
+#Exponenciação
+exponenciacao = 2 ** 10
+print(exponenciacao)
+#Modulo
+#0 = Quando o numerador, é divisível pelo denominador 
+#1 = Quando o numerador, NÃO é divisível pelo denominador 
+modulo = 55 % 2 #Resto da divisão
+print(modulo)
+#Multiplo = quando um é divisivel pelo outro e retorna 0
+print(16 % 8 == 0) # True
+print(10 % 8 == 0) # False, retorna 1
+#Par ou impar
+print(10 % 2 == 0)# True, então é par
+print(15 % 2 == 0)# False, então é impar
