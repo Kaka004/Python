0583442d47a04671ffc8d53a9a937acaea153029
Kayo Vinícius <kayo.vinicius034@gmail.com>
2024-10-30 10:16:30 -0300

    Uso do print
    Usado para exibir algo na tela, podendo colocar Argumentos e Métodos. Falando sobre a tipagem da Linguagem Python, que é dinâmica e forte

---
+++ Curso Python Udemy/Aula Basica/print.py
@@ -0,0 +1,26 @@
+"""
+Aula Voltada ao uso do print
+    Print é uma função que usada,
+    exibe algo na tela
+    'print()'
+        Dentro do print, posso colocar:
+        - Argumentos e métodos..
+        
+        Python é uma linguagem de programação
+        com uma tipagem dinâmica e forte
+"""
+# print(12, 34, sep=".") # Argumentos NÃO nomeados
+# print(56, 78, sep='-') # Separador
+
+# # Escapae
+# print("Kayo \"Vinicius")
+# # r
+# print(r"Kayo \"Vinicius\"")
+
+# Função para saber o tipo em que o Python inseriu o valor
+print(type('Kayo'))
+print(type(1))
+print(type(1.1), type(-1.1), type(0.0))
+
+#Classes começam com letra maiúscula
+#Funções começam com letra minúscula
