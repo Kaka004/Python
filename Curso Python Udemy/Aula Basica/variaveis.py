7413b84586aa15e5475f1ccfa7e86ef26982d7be
Kayo Vinícius <kayo.vinicius034@gmail.com>
2024-10-30 10:08:56 -0300

    Variaveis
    Aula sobre variáveis abordando sobre o que é variável, como são escritos, sinal de atribuição e PEP8

---
+++ Curso Python Udemy/Aula Basica/variaveis.py
@@ -0,0 +1,15 @@
+#Variaveis são usadass para salvar algo na memoria do computador
+#PEP8: Iniciar com variaveis com letras minusculas, pode usar 
+#números e underline_
+# = é um sinal de atribuição. Usado para atribuir um valor a um nome (variável) 
+#Uso: nome_variavel = expressão
+
+nome_completo = 'Kayo Vinícius Amaro Benjamin'
+soma_dois_mais_dois = 2 + 2
+print(nome_completo, soma_dois_mais_dois) #Kayo Vinícius Amaro Benjamin
+
+nome = 'Kayo'
+idade = 17
+maiorIdade = idade >= 18
+print("Nome: {} Idade: {}".format(nome, idade))
+print("É maior de idade? {}".format(maiorIdade))
