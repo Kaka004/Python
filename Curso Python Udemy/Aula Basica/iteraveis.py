c51992e7ccc8e807b739fa3abbac11717171aef9
Kayo Vinícius <kayo.vinicius034@gmail.com>
2024-10-30 10:27:32 -0300

    Iteráveis
    Aula aborda sobre o que são strings iteraveis ( in e not in ) e como usá-los

---
+++ Curso Python Udemy/Aula Basica/iteraveis.py
@@ -0,0 +1,13 @@
+#Strings são iteráveis
+
+# in = esta entre
+# not in = não está entre
+
+# 0 1 2 3 4 5 
+# o t a v i o
+# -6-5-4-3-2-1
+
+nome = 'otavio'
+print('a' in nome)
+print(10 * '-') 
+print(nome[3])
