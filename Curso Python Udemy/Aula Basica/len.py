bc8b2821a716d72c5e0fbec2b4a5f8bd4f8d00be
Kayo Vinícius <kayo.vinicius034@gmail.com>
2024-10-30 10:26:11 -0300

    Função "len" e fatiamento
    Aula aborda como usar a Função len, o que ela irá retornar, o que é fatiamento, como aplicar tambem

---
+++ Curso Python Udemy/Aula Basica/len.py
@@ -0,0 +1,14 @@
+"""
+Fatiamento de Strings
+
+ 012345678
+ ola mundo
+-987654321
+
+fatiamento [i:f:p] [::]
+Obs: a função len retorna a quantidade
+de caracteres da str
+"""
+
+variavel = 'Hello World'
+print(len(variavel))
