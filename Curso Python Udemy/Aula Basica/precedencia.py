6a7fb47eb4f6cdeb8302fe81c53ae49fb1719be3
Kayo Vinícius <kayo.vinicius034@gmail.com>
2024-10-30 10:20:24 -0300

    Procedência dos operadores
    Nessa aula falamos sobre a procedência de cada operador, quais são os primeiros que o programa irá executar etc.

---
+++ Curso Python Udemy/Aula Basica/precedencia.py
@@ -0,0 +1,6 @@
+#1. (n + n) Parenteses
+#2. ** Exponenciação
+#3. * / // % Multiplicaação, Divisão, Divisão Inteira e Modulo (Resto da Divisão)
+#$ + - Adição e Subtração
+conta = 1 + 1 ** 5 + 5
+print(conta)
