81725ab262b991bfa99dd9824460035e97f2bb00
Kayo Vinícius <kayo.vinicius034@gmail.com>
2024-10-30 10:14:24 -0300

    Tipos de Variaveis
    Aula abordada sobre os tipos de variáveis que tem na Linguagem Python, Coerção, Conversão de Tipos e Tipos Imutáveis e Primitivos

---
+++ Curso Python Udemy/Aula Basica/tipos.py
@@ -0,0 +1,17 @@
+# conversão de tipos, coerção
+# type coversion, type casting, coersion
+#é o ato de converter um tipo em outro
+# tipos imutáveis e primitivos
+# str, int, float, bool
+
+#print('1' + 1) # Erro! Uma string não pode somar com  um inteiro
+print('a' + 'b') #Estou fazendo uma concatenação. A resultado vai ser "ab"
+
+# String para inteiro
+print(int('1'), type(int('1')))
+# String para float
+print(type(float('1.2') + 1))
+#Para Booleano
+print(bool(' '))
+#Para String
+print(str(11) + 'b')
