b4a5b4fd07bae5b8e6c1e5aa7c1b4052fb44d4c7
Kayo Vinícius <kayo.vinicius034@gmail.com>
2024-10-30 10:34:49 -0300

    Como usar a Concatenação
    Aula abordada em como usar a concatenação e dando exemplos

---
+++ Curso Python Udemy/Aula Basica/concatenacao.py
@@ -0,0 +1,9 @@
+#Podemos usar o sinal de + (mais) para fazer a concatenação
+#Exemplo:
+concatenacao = 'Kayo' + ' ' + 'Benjamin'
+print(concatenacao)
+#Aparecer a letra inúmeras vezes; Fazer repetições
+aDezVezes = 'A' * 10
+TresVezesKayo = 3 * 'Kayo'
+print(aDezVezes)
+print(TresVezesKayo)
