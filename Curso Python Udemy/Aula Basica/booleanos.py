64f4b1ab3ec38596f1e93796a2759a1971e1f05c
Kayo Vinícius <kayo.vinicius034@gmail.com>
2024-10-30 10:35:51 -0300

    Booleanos
    Aula abordada em o que é um booleano, o que ele retorna e como ele é aplicado

---
+++ Curso Python Udemy/Aula Basica/booleanos.py
@@ -0,0 +1,10 @@
+#Os booleanos tem só duas possíveis respostas
+#sim (True) ou não(False)
+
+# print(10 == 10) #True
+# print(10 == 20) #False
+# print(10 != 20) #True
+# print(10 > 20) #False
+# print(10 < 20) #True
+# print(10 <= 10) # True
+# print(10 >= 10) # True
