47d8ea6729a9be5a6ca1975effd0a17972c610e6
Kayo Vinícius <kayo.vinicius034@gmail.com>
2024-10-30 10:22:09 -0300

    Operadores relacionais
    Aula aborda sobre os operadores de comparação, de como é usado na Linguagem Python. Como esta no codigo, ira retornar um booleano (True or False)

---
+++ Curso Python Udemy/Aula Basica/operadores_relacionais.py
@@ -0,0 +1,18 @@
+"""
+Operadores de comparação (relacionais)
+
+>   maior que     2 > 1
+>= maior igual    2 >= 2
+<   menor que     1 < 2
+<= menor igual    2 <= 2
+==  igual         2 == 2
+!=  diferente     1 != 2
+"""
+maior = 2 > 1
+maiorIgual = 2 >= 2
+menor = 1 < 2
+menorIgual = 2 <= 2
+igual = 2 == 2
+diferente = 1 != 2
+
+print(maiorIgual)
